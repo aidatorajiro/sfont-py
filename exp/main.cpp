93fd6bc9be54255098dd5038a6498ac25033264f
aidatorajiro <kawarusosu@zoho.com>
2021-02-09 16:40:26 +0900

    add type size check

--- exp/main.cpp
+++ exp/main.cpp
@@ -4,9 +4,27 @@
 #include "sfont.cpp"
 
 int main(int argc, char *argv[]){
-  const QString &qs = QString("aaa");
+  const QString &qs = QString("../FluidR3_GM.sf2");
 
-  new SfTools::SoundFont(qs);
+  SfTools::SoundFont *sf = new SfTools::SoundFont(qs);
+  sf->read();
+  
+  printf("=== TYPE SIZES ===\n");
+  
+  printf("sfVersionTag: %d\n", sizeof(sfVersionTag));
+  printf("Modulator: %d\n", sizeof(Modulator));
+  printf("Generator: %d\n", sizeof(Generator));
+  printf("Transform: %d\n", sizeof(Transform));
+  printf("ModulatorList: %d\n", sizeof(ModulatorList));
+  printf("GeneratorAmount: %d\n", sizeof(GeneratorAmount));
+  printf("GeneratorList: %d\n", sizeof(GeneratorList));
+  printf("Zone: %d\n", sizeof(Zone));
+  printf("Preset: %d\n", sizeof(Preset));
+  printf("Instrument: %d\n", sizeof(Instrument));
+  printf("Sample: %d\n", sizeof(Sample));
+  printf("SoundFont: %d\n", sizeof(SoundFont));
+  printf("QString: %d\n", sizeof(QString));
+  printf("QList: %d\n", sizeof(QList<QString>));
   
   return 0;
 }
