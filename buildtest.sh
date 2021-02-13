fd814c2319cfe4ece5044c5fe137fc427af46cd9
aidatorajiro <kawarusosu@zoho.com>
2021-02-13 20:13:28 +0900

    split source files

--- buildtest.sh
+++ buildtest.sh
@@ -1,2 +1,2 @@
-clang++ -dynamiclib -L/usr/local/lib -logg -lvorbis -lvorbisfile -lvorbisenc -I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include -F/usr/local/opt/qt/lib -framework QtCore -std=c++11 main.cpp -o sfont.dylib
+clang++ -dynamiclib -L/usr/local/lib -logg -lvorbis -lvorbisfile -lvorbisenc -I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include -F/usr/local/opt/qt/lib -framework QtCore -std=c++11 interface.cpp -o sfont.dylib
 python3 test.py
