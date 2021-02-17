12bbfc24d025c7cc01b5c33754fe096dcb01dbef
aidatorajiro <kawarusosu@zoho.com>
2021-02-17 14:48:53 +0900

    use python unittest

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import unittest
 from sfont import *
 import os
 import urllib.request
@@ -7,31 +8,38 @@ import io
 if not os.path.exists("FluidR3_GM.sf2"):
     url = "http://deb.debian.org/debian/pool/main/f/fluid-soundfont/fluid-soundfont_3.1.orig.tar.gz"
 
-    print("downloading " + url + "...")
+    print("Downloading " + url + "...")
 
     with urllib.request.urlopen(url) as f:
         compressed = f.read()
-    
+
     bio = io.BytesIO(compressed)
 
     tar = tarfile.open(fileobj=bio, mode="r:gz")
 
     data = tar.extractfile("fluid-soundfont-3.1/FluidR3_GM.sf2").read()
 
+    print("Writing to FluidR3_GM.sf2...")
+
     with open("FluidR3_GM.sf2", "wb") as f:
         f.write(data)
 
-print("sfVersionTag: %d" % ctypes.sizeof(sfVersionTag))
-print("ModulatorList: %d" % ctypes.sizeof(ModulatorList))
-print("GeneratorAmount: %d" % ctypes.sizeof(GeneratorAmount))
-print("GeneratorList: %d" % ctypes.sizeof(GeneratorList))
-print("Zone: %d" % ctypes.sizeof(Zone))
-print("Preset: %d" % ctypes.sizeof(Preset))
-print("Instrument: %d" % ctypes.sizeof(Instrument))
-print("Sample: %d" % ctypes.sizeof(Sample))
-print("SoundFont: %d" % ctypes.sizeof(SoundFont))
+class SfontTest(unittest.TestCase):
+    def test_type(self):
+        self.assertEqual(ctypes.sizeof(sfVersionTag), 8)
+        self.assertEqual(ctypes.sizeof(ModulatorList), 20)
+        self.assertEqual(ctypes.sizeof(GeneratorAmount), 2)
+        self.assertEqual(ctypes.sizeof(GeneratorList), 8)
+        self.assertEqual(ctypes.sizeof(Zone), 24)
+        self.assertEqual(ctypes.sizeof(Preset), 40)
+        self.assertEqual(ctypes.sizeof(Instrument), 24)
+        self.assertEqual(ctypes.sizeof(Sample), 48)
+        self.assertEqual(ctypes.sizeof(SoundFont), 144)
 
-sf = sfont.read_sound_font(b"FluidR3_GM.sf2").contents
+    def test_data(self):
+        sf = sfont.read_sound_font(b"FluidR3_GM.sf2").contents
+        self.assertEqual(sf.presets.size(), 189)
+        self.assertEqual(sf.presets.array()[10].contents.name, b'Burst Noise')
 
-print(sf.presets.size() == 189)
-print(sf.presets.array()[10].contents.name == b'Burst Noise')
+if __name__ == '__main__':
+    unittest.main()
