8ae3cfa742e41426fcddd0e071cf0f3c59db8051
aidatorajiro <kawarusosu@zoho.com>
2021-02-17 14:30:39 +0900

    download FluidR3_GM.sf2 from debian server

--- test.py
+++ test.py
@@ -1,4 +1,25 @@
 from sfont import *
+import os
+import urllib.request
+import tarfile
+import io
+
+if not os.path.exists("FluidR3_GM.sf2"):
+    url = "http://deb.debian.org/debian/pool/main/f/fluid-soundfont/fluid-soundfont_3.1.orig.tar.gz"
+
+    print("downloading " + url + "...")
+
+    with urllib.request.urlopen(url) as f:
+        compressed = f.read()
+    
+    bio = io.BytesIO(compressed)
+
+    tar = tarfile.open(fileobj=bio, mode="r:gz")
+
+    data = tar.extractfile("fluid-soundfont-3.1/FluidR3_GM.sf2").read()
+
+    with open("FluidR3_GM.sf2", "wb") as f:
+        f.write(data)
 
 print("sfVersionTag: %d" % ctypes.sizeof(sfVersionTag))
 print("ModulatorList: %d" % ctypes.sizeof(ModulatorList))
